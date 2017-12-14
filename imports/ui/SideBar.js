320bdca2010dfda402d3d933d045c7c5e2b30303
Gerardo Bautista <gerardoemery@live.com.mx>
2017-12-13 22:18:31 -0600

    grid

--- imports/ui/SideBar.js
+++ imports/ui/SideBar.js
@@ -3,15 +3,33 @@ import React from 'react';
 class SideBar extends React.Component {
 	render() {
 			var estilo = { textDecoration: "underline" };
-			return (<div className='sidebar-puntos'> 
-			<h1>habitos</h1>
+			return (
+			<div className='sidebar-puntos'> 
+			
+			<div className='grid'>
+			<h1><span>h</span>abitos</h1>
 			<ul> <li> No a las distracciones </li> 
 							<li> Leer algo nuevo todos los dias. </li>
 							<li> Acepta tus errores y sigue adelante. </li>
 							<li> Acostumbrate a levantarte temprano. </li>
 							<li style={estilo}>Cuida tu cuerpo</li>
 							<li> aprender a delegar </li>
-					</ul></div> 
+			</ul>
+			</div>
+			
+			<div className='grid'>
+			<h1><span>p</span>ensamientos</h1>
+			<ul> <li>tal vez solo un cuento trivial, un pensamiento que sentó en diagonal</li>	
+							<li>sin tener en que caerme y con el cuerpo abatido</li>
+							<li>mi movimiento asistido por un pensamiento abstraido</li>
+							<li>influenciado en el vecindario, estas palabras no son del armario</li>
+							<li>solo doy mi punto de vista,  un renglon en el temario </li> 
+			</ul>
+			</div>
+
+			
+			</div> 
+			
 		
 			);
 	};
